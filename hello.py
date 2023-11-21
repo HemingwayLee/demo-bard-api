813f50c9f2541a7690e9cc6fd8e63d233ca29d02
HemingwayLee <coherence0815@gmail.com>
2023-11-21 10:09:29 +0900

    update example

--- hello.py
+++ hello.py
@@ -3,11 +3,12 @@ from bardapi import BardCookies
 
 cookie_dict = {
     "__Secure-1PSID": "cghBBIw5QpAvtqPuMQuFkIlk8RGX6gU0QDHSuIk0oi20Umn-dc8M8Glfzf65ru_YWl5rgQ.",
-    "__Secure-1PSIDTS": "sidts-CjEBNiGH7gqctZrl14Kcm6h9DWjb3KI8riGLREF0P0f4YusXthosA787QGMsbDvhBrMNEAA",
+    "__Secure-1PSIDTS": "sidts-CjEBNiGH7sFZ-lzxSL4iFfvDSg2PlnOltF8qTfy3in5Df4wHMRXPruqDbh3OPvcC3lqnEAA",
     # Any cookie values you want to pass session object.
 }
 
 bard = BardCookies(cookie_dict=cookie_dict)
 
 print(bard.get_answer("こんにちは"))
+print(bard.get_answer("give me a Japanese sentence which contains 'ラーメン'")['content'])
 
